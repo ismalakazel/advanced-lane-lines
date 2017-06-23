bda4b0825329c71dab5cdbfdc2d64432089011f6
Israel Tavares <ismalakazel@gmail.com>
2017-06-23 13:01:20 -0300

    Add aditional images

--- gradient_threshold.py
+++ gradient_threshold.py
@@ -2,23 +2,18 @@ import numpy as np
 import cv2
 import sobel
 import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 
 IMAGE_PATH = './assets/test_images/test3.jpg'
 IMG = mpimg.imread(IMAGE_PATH)
 HEIGHT, WIDTH, _ = IMG.shape
 
 def binary_image(img=IMG):
-    v = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)[:, :, 2]
+    v = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)[:, :, 2]
     filter1 = sobel.binary_sobel(v, 15, 200)
 
-    luv_v = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)[:, :, 2]
-    filter2 = sobel.binary(luv_v, 165, 255)
+    return filter1
 
-    l = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)[:, :, 1]
-    filter3 = (sobel.binary(l, 190, 255))
 
-    combined = np.zeros_like(filter1)
-    combined[(filter1 == 1) | (filter2 == 1)| (filter3 == 1)] = 1
-
-    return combined
-
+plt.imshow(binary_image(), cmap="gray")
+plt.show()
